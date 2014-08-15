759ce81d789577cb016f8230b57ed52b0a489630
Moritz Kammerer <kammerer.moritz@gmail.com>
2014-08-15 20:48:53 +0200

    Starting a unit of work in the clock.

--- rest/src/main/java/restwars/rest/Clock.java
+++ rest/src/main/java/restwars/rest/Clock.java
@@ -4,12 +4,14 @@ import com.google.common.base.Preconditions;
 import io.dropwizard.lifecycle.Managed;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import restwars.UnrecoverableException;
 import restwars.service.UniverseConfiguration;
 import restwars.service.building.BuildingService;
 import restwars.service.infrastructure.RoundService;
 import restwars.service.resource.ResourceService;
 import restwars.service.ship.ShipService;
 import restwars.service.technology.TechnologyService;
+import restwars.service.unitofwork.UnitOfWorkService;
 
 import javax.annotation.Nullable;
 import javax.inject.Inject;
@@ -27,11 +29,14 @@ public class Clock implements Managed, Runnable {
     private final ShipService shipService;
     private final UniverseConfiguration universeConfiguration;
 
+    private final UnitOfWorkService unitOfWorkService;
+
     @Nullable
     private ScheduledExecutorService scheduledExecutorService;
 
     @Inject
-    public Clock(BuildingService buildingService, RoundService roundService, UniverseConfiguration universeConfiguration, ResourceService resourceService, TechnologyService technologyService, ShipService shipService) {
+    public Clock(BuildingService buildingService, RoundService roundService, UniverseConfiguration universeConfiguration, ResourceService resourceService, TechnologyService technologyService, ShipService shipService, UnitOfWorkService unitOfWorkService) {
+        this.unitOfWorkService = Preconditions.checkNotNull(unitOfWorkService, "unitOfWorkService");
         this.shipService = Preconditions.checkNotNull(shipService, "shipService");
         this.technologyService = Preconditions.checkNotNull(technologyService, "technologyService");
         this.resourceService = Preconditions.checkNotNull(resourceService, "resourceService");
@@ -57,13 +62,21 @@ public class Clock implements Managed, Runnable {
 
     @Override
     public void run() {
-        long round = roundService.nextRound();
-        LOGGER.info("Starting round {}", round);
+        unitOfWorkService.start();
+        try {
+            long round = roundService.nextRound();
+            LOGGER.info("Starting round {}", round);
 
-        buildingService.finishConstructionSites();
-        technologyService.finishResearches();
-        shipService.finishShipsInConstruction();
-        resourceService.gatherResourcesOnAllPlanets();
-        shipService.finishFlights();
+            buildingService.finishConstructionSites();
+            technologyService.finishResearches();
+            shipService.finishShipsInConstruction();
+            resourceService.gatherResourcesOnAllPlanets();
+            shipService.finishFlights();
+
+            unitOfWorkService.commit();
+        } catch (Exception e) {
+            unitOfWorkService.abort();
+            throw new UnrecoverableException("Exception", e);
+        }
     }
 }
