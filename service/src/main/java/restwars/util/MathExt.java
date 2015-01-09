cbe99311c38c30a7ffa5368388e98ba252a18ceb
Moritz Kammerer <kammerer.moritz@gmail.com>
2015-01-09 22:17:14 +0100

    Documentation.

--- service/src/main/java/restwars/util/MathExt.java
+++ service/src/main/java/restwars/util/MathExt.java
@@ -1,13 +1,31 @@
 package restwars.util;
 
+/**
+ * Extended Math functions.
+ */
 public final class MathExt {
+    /**
+     * No instances allowed.
+     */
     private MathExt() {
     }
 
+    /**
+     * A {@link Math#ceil(double)} with a long result.
+     *
+     * @param value Value to ceil.
+     * @return Result.
+     */
     public static long ceilLong(double value) {
         return (long) Math.ceil(value);
     }
 
+    /**
+     * A {@link Math#floor(double)} with a long result.
+     *
+     * @param value Value to floor.
+     * @return Result.
+     */
     public static long floorLong(double value) {
         return (long) Math.floor(value);
     }
