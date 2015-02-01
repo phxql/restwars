b41061c01672847aba2b62d73d8c8071b0ae6a3a
Moritz Kammerer <kammerer.moritz@gmail.com>
2015-02-01 18:28:28 +0100

    Tests for BuildingServiceImpl.

--- service/src/test/java/restwars/service/building/impl/BuildingServiceImplTest.java
+++ service/src/test/java/restwars/service/building/impl/BuildingServiceImplTest.java
@@ -23,6 +23,7 @@ import restwars.service.technology.TechnologyDAO;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Optional;
 import java.util.UUID;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -55,6 +56,30 @@ public class BuildingServiceImplTest {
         buildingMechanics = mock(BuildingMechanics.class);
         technologyMechanics = mock(TechnologyMechanics.class);
 
+        // Prerequisites, build cost and build time for command center
+        when(buildingMechanics.getPrerequisites(BuildingType.COMMAND_CENTER)).thenReturn(Prerequisites.NONE);
+        when(buildingMechanics.calculateBuildCost(BuildingType.COMMAND_CENTER, 1)).thenReturn(new Resources(5, 10, 15));
+        when(buildingMechanics.calculateBuildCost(BuildingType.COMMAND_CENTER, 2)).thenReturn(new Resources(10, 20, 30));
+        when(buildingMechanics.calculateBuildTime(BuildingType.COMMAND_CENTER, 1)).thenReturn(1);
+        when(buildingMechanics.calculateBuildTime(BuildingType.COMMAND_CENTER, 2)).thenReturn(2);
+        // Prerequisites, build cost and build time for crystal mine
+        when(buildingMechanics.getPrerequisites(BuildingType.CRYSTAL_MINE)).thenReturn(Prerequisites.NONE);
+        when(buildingMechanics.calculateBuildCost(BuildingType.CRYSTAL_MINE, 1)).thenReturn(new Resources(5, 10, 15));
+        when(buildingMechanics.calculateBuildCost(BuildingType.CRYSTAL_MINE, 2)).thenReturn(new Resources(10, 20, 30));
+        when(buildingMechanics.calculateBuildTime(BuildingType.CRYSTAL_MINE, 1)).thenReturn(1);
+        when(buildingMechanics.calculateBuildTime(BuildingType.CRYSTAL_MINE, 2)).thenReturn(2);
+        // Build cost reduction through technology
+        when(technologyMechanics.calculateBuildCostReduction(0)).thenReturn(0.0);
+        // Technologies for player 1
+        when(technologyDAO.findAllWithPlayerId(Data.Player1.PLAYER.getId())).thenReturn(Technologies.NONE);
+        // Buildings on planet 1 from player 1
+        when(buildingDAO.findWithPlanetId(Data.Player1.Planet1.PLANET.getId())).thenReturn(new Buildings(Data.Player1.Planet1.COMMAND_CENTER));
+        when(buildingDAO.findWithPlanetIdAndType(Data.Player1.Planet1.PLANET.getId(), BuildingType.COMMAND_CENTER)).thenReturn(Optional.of(Data.Player1.Planet1.COMMAND_CENTER));
+        // No construction sites in planet 1 from player 1
+        when(constructionSiteDAO.findWithPlanetId(Data.Player1.Planet1.PLANET.getId())).thenReturn(Collections.emptyList());
+        // We are at round 1
+        when(roundService.getCurrentRound()).thenReturn(1L);
+
         sut = new BuildingServiceImpl(uuidFactory, buildingDAO, roundService, constructionSiteDAO, planetDAO, eventService, technologyDAO, buildingMechanics, technologyMechanics);
     }
 
@@ -84,25 +109,13 @@ public class BuildingServiceImplTest {
 
     @Test
     public void testConstructBuilding() throws Exception {
-        when(buildingMechanics.getPrerequisites(BuildingType.COMMAND_CENTER)).thenReturn(Prerequisites.NONE);
-        when(technologyDAO.findAllWithPlayerId(Data.Player1.Planet1.PLANET.getOwnerId())).thenReturn(Technologies.NONE);
-        when(buildingDAO.findWithPlanetId(Data.Player1.Planet1.PLANET.getId())).thenReturn(new Buildings(Data.Player1.Planet1.COMMAND_CENTER));
-
-        when(constructionSiteDAO.findWithPlanetId(Data.Player1.Planet1.PLANET.getId())).thenReturn(Collections.emptyList());
-
-        when(buildingMechanics.calculateBuildCost(BuildingType.COMMAND_CENTER, 1)).thenReturn(new Resources(10, 20, 30));
-        when(technologyMechanics.calculateBuildCostReduction(0)).thenReturn(0.0);
-
-        when(buildingMechanics.calculateBuildTime(BuildingType.COMMAND_CENTER, 1)).thenReturn(1);
-
         UUID id = UUID.randomUUID();
         when(uuidFactory.create()).thenReturn(id);
-        when(roundService.getCurrentRound()).thenReturn(1L);
 
-        ConstructionSite expected = new ConstructionSite(id, BuildingType.COMMAND_CENTER, 1, Data.Player1.Planet1.PLANET.getId(), Data.Player1.PLAYER.getId(), 1, 2);
-        ConstructionSite actual = sut.constructBuilding(Data.Player1.Planet1.PLANET, BuildingType.COMMAND_CENTER);
+        ConstructionSite expected = new ConstructionSite(id, BuildingType.CRYSTAL_MINE, 1, Data.Player1.Planet1.PLANET.getId(), Data.Player1.PLAYER.getId(), 1, 2);
+        ConstructionSite actual = sut.constructBuilding(Data.Player1.Planet1.PLANET, BuildingType.CRYSTAL_MINE);
 
-        verify(planetDAO).update(Data.Player1.Planet1.PLANET.withResources(Resources.NONE));
+        verify(planetDAO).update(Data.Player1.Planet1.PLANET.withResources(new Resources(5, 10, 15)));
         verify(constructionSiteDAO).insert(expected);
 
         assertThat(expected, is(actual));
@@ -110,9 +123,8 @@ public class BuildingServiceImplTest {
 
     @Test(description = "Prerequisites not fulfilled")
     public void testConstructBuilding2() throws Exception {
+        // Change prerequisites for command center
         when(buildingMechanics.getPrerequisites(BuildingType.COMMAND_CENTER)).thenReturn(Prerequisites.buildings(new Prerequisites.Building(BuildingType.SHIPYARD, 10)));
-        when(technologyDAO.findAllWithPlayerId(Data.Player1.Planet1.PLANET.getOwnerId())).thenReturn(Technologies.NONE);
-        when(buildingDAO.findWithPlanetId(Data.Player1.Planet1.PLANET.getId())).thenReturn(new Buildings(Data.Player1.Planet1.COMMAND_CENTER));
 
         try {
             sut.constructBuilding(Data.Player1.Planet1.PLANET, BuildingType.COMMAND_CENTER);
@@ -124,11 +136,9 @@ public class BuildingServiceImplTest {
 
     @Test(description = "Not enough build queues")
     public void testConstructBuilding3() throws Exception {
-        when(buildingMechanics.getPrerequisites(BuildingType.COMMAND_CENTER)).thenReturn(Prerequisites.NONE);
-        when(technologyDAO.findAllWithPlayerId(Data.Player1.Planet1.PLANET.getOwnerId())).thenReturn(Technologies.NONE);
-        when(buildingDAO.findWithPlanetId(Data.Player1.Planet1.PLANET.getId())).thenReturn(new Buildings(Data.Player1.Planet1.COMMAND_CENTER));
-
+        // One building in the build queue for planet 1 from player 1
         when(constructionSiteDAO.findWithPlanetId(Data.Player1.Planet1.PLANET.getId())).thenReturn(Arrays.asList(Data.Player1.Planet1.CONSTRUCTION_SITE));
+
         try {
             sut.constructBuilding(Data.Player1.Planet1.PLANET, BuildingType.COMMAND_CENTER);
             fail("Expected exception");
@@ -139,39 +149,91 @@ public class BuildingServiceImplTest {
 
     @Test(description = "Insufficient resoyrces")
     public void testConstructBuilding4() throws Exception {
-        when(buildingMechanics.getPrerequisites(BuildingType.COMMAND_CENTER)).thenReturn(Prerequisites.NONE);
-        when(technologyDAO.findAllWithPlayerId(Data.Player1.Planet1.PLANET.getOwnerId())).thenReturn(Technologies.NONE);
-        when(buildingDAO.findWithPlanetId(Data.Player1.Planet1.PLANET.getId())).thenReturn(new Buildings(Data.Player1.Planet1.COMMAND_CENTER));
-        when(constructionSiteDAO.findWithPlanetId(Data.Player1.Planet1.PLANET.getId())).thenReturn(Collections.emptyList());
-
+        // Change build cost for command center
         when(buildingMechanics.calculateBuildCost(BuildingType.COMMAND_CENTER, 1)).thenReturn(new Resources(1000, 2000, 3000));
+
         try {
             sut.constructBuilding(Data.Player1.Planet1.PLANET, BuildingType.COMMAND_CENTER);
             fail("Expected exception");
         } catch (BuildingException e) {
             assertThat(e.getReason(), is(BuildingException.Reason.INSUFFICIENT_RESOURCES));
         }
-
     }
 
     @Test
     public void testUpgradeBuilding() throws Exception {
+        UUID id = UUID.randomUUID();
+        when(uuidFactory.create()).thenReturn(id);
 
+        ConstructionSite expected = new ConstructionSite(id, BuildingType.COMMAND_CENTER, 2, Data.Player1.Planet1.PLANET.getId(), Data.Player1.PLAYER.getId(), 1, 3);
+        ConstructionSite actual = sut.upgradeBuilding(Data.Player1.Planet1.PLANET, BuildingType.COMMAND_CENTER);
+
+        verify(planetDAO).update(Data.Player1.Planet1.PLANET.withResources(new Resources(0, 0, 0)));
+        verify(constructionSiteDAO).insert(expected);
+
+        assertThat(expected, is(actual));
     }
 
-    @Test
+    @Test(description = "Construct building")
     public void testConstructOrUpgradeBuilding() throws Exception {
+        UUID id = UUID.randomUUID();
+        when(uuidFactory.create()).thenReturn(id);
 
+        ConstructionSite expected = new ConstructionSite(id, BuildingType.CRYSTAL_MINE, 1, Data.Player1.Planet1.PLANET.getId(), Data.Player1.PLAYER.getId(), 1, 2);
+        ConstructionSite actual = sut.constructOrUpgradeBuilding(Data.Player1.Planet1.PLANET, BuildingType.CRYSTAL_MINE);
+
+        verify(planetDAO).update(Data.Player1.Planet1.PLANET.withResources(new Resources(5, 10, 15)));
+        verify(constructionSiteDAO).insert(expected);
+
+        assertThat(expected, is(actual));
+    }
+
+    @Test(description = "Upgrade building")
+    public void testConstructOrUpgradeBuilding2() throws Exception {
+        UUID id = UUID.randomUUID();
+        when(uuidFactory.create()).thenReturn(id);
+
+        ConstructionSite expected = new ConstructionSite(id, BuildingType.COMMAND_CENTER, 2, Data.Player1.Planet1.PLANET.getId(), Data.Player1.PLAYER.getId(), 1, 3);
+        ConstructionSite actual = sut.constructOrUpgradeBuilding(Data.Player1.Planet1.PLANET, BuildingType.COMMAND_CENTER);
+
+        verify(planetDAO).update(Data.Player1.Planet1.PLANET.withResources(new Resources(0, 0, 0)));
+        verify(constructionSiteDAO).insert(expected);
+
+        assertThat(expected, is(actual));
     }
 
     @Test
     public void testCalculateBuildCostWithoutBonuses() throws Exception {
+        Resources costLevel1 = sut.calculateBuildCostWithoutBonuses(BuildingType.COMMAND_CENTER, 1);
+        assertThat(costLevel1, is(new Resources(5, 10, 15)));
 
+        Resources costLevel2 = sut.calculateBuildCostWithoutBonuses(BuildingType.COMMAND_CENTER, 2);
+        assertThat(costLevel2, is(new Resources(10, 20, 30)));
     }
 
-    @Test
+    @Test(description = "Constructs a new shipyard")
     public void testFinishConstructionSites() throws Exception {
+        UUID id = UUID.randomUUID();
+        when(uuidFactory.create()).thenReturn(id);
+        when(constructionSiteDAO.findWithDone(1)).thenReturn(Arrays.asList(Data.Player1.Planet1.CONSTRUCTION_SITE));
 
+        sut.finishConstructionSites();
+
+        Building expected = new Building(id, BuildingType.SHIPYARD, 1, Data.Player1.Planet1.PLANET.getId());
+        verify(buildingDAO).insert(expected);
+    }
+
+    @Test(description = "Upgrades the command center")
+    public void testFinishConstructionSites2() throws Exception {
+        UUID id = UUID.randomUUID();
+        when(uuidFactory.create()).thenReturn(id);
+        ConstructionSite constructionSite = new ConstructionSite(UUID.randomUUID(), BuildingType.COMMAND_CENTER, 2, Data.Player1.Planet1.PLANET.getId(), Data.Player1.PLAYER.getId(), 1, 1);
+        when(constructionSiteDAO.findWithDone(1)).thenReturn(Arrays.asList(constructionSite));
+
+        sut.finishConstructionSites();
+
+        Building expected = Data.Player1.Planet1.COMMAND_CENTER.withLevel(2);
+        verify(buildingDAO).update(expected);
     }
 
     @Test
