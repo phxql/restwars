ac8271ceca5105c77fc52c3f5a0374d8d8838083
Moritz Kammerer <kammerer.moritz@gmail.com>
2015-02-09 21:09:39 +0100

    Tests for RootResource.

--- rest/src/test/java/restwars/rest/resources/PlayerResourceTest.java
+++ rest/src/test/java/restwars/rest/resources/PlayerResourceTest.java
@@ -1,15 +1,12 @@
 package restwars.rest.resources;
 
-import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.api.client.filter.HTTPBasicAuthFilter;
-import io.dropwizard.auth.basic.BasicAuthProvider;
 import io.dropwizard.testing.junit.ResourceTestRule;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Test;
 import restwars.Data;
-import restwars.rest.DummyAuthenticator;
 import restwars.restapi.dto.planet.PlanetResponse;
 import restwars.restapi.dto.player.PlayerResponse;
 import restwars.restapi.dto.player.RegisterPlayerRequest;
@@ -17,7 +14,6 @@ import restwars.service.planet.PlanetService;
 import restwars.service.player.PlayerService;
 
 import javax.validation.ConstraintViolationException;
-import javax.ws.rs.core.MediaType;
 import java.util.Arrays;
 import java.util.List;
 
@@ -26,14 +22,13 @@ import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.*;
 
-public class PlayerResourceTest {
+public class PlayerResourceTest extends AbstractResourceTest {
     public static final String URL = "/v1/player";
     private static PlayerService playerService = mock(PlayerService.class);
     private static PlanetService planetService = mock(PlanetService.class);
 
     @ClassRule
-    public static final ResourceTestRule resources = ResourceTestRule.builder()
-            .addProvider(new BasicAuthProvider<>(new DummyAuthenticator("username", "password", Data.Player1.PLAYER), "test"))
+    public static final ResourceTestRule resources = createRule()
             .addResource(new PlayerResource(playerService, planetService))
             .build();
 
@@ -49,34 +44,30 @@ public class PlayerResourceTest {
 
     @Test
     public void testRegister() throws Exception {
-        request(URL).post(new RegisterPlayerRequest("username", "password"));
+        request(resources, URL).post(new RegisterPlayerRequest("username", "password"));
 
         verify(playerService).createPlayer("username", "password");
     }
 
     @Test(expected = ConstraintViolationException.class)
     public void testRegister2() throws Exception {
-        request(URL).post(new RegisterPlayerRequest("", "password"));
+        request(resources, URL).post(new RegisterPlayerRequest("", "password"));
     }
 
     @Test(expected = ConstraintViolationException.class)
     public void testRegister3() throws Exception {
-        request(URL).post(new RegisterPlayerRequest("username", ""));
+        request(resources, URL).post(new RegisterPlayerRequest("username", ""));
     }
 
     @Test
     public void testMe() throws Exception {
         when(planetService.findWithOwner(Data.Player1.PLAYER)).thenReturn(Arrays.asList(Data.Player1.Planet1.PLANET));
 
-        PlayerResponse response = request(URL).get(PlayerResponse.class);
+        PlayerResponse response = request(resources, URL).get(PlayerResponse.class);
 
         assertThat(response.getUsername(), is("username"));
         List<PlanetResponse> planets = response.getPlanets();
         assertThat(planets, hasSize(1));
         assertThat(planets.get(0), is(new PlanetResponse("1.1.1", 100, 200, 300)));
-    }
-
-    private WebResource.Builder request(String url) {
-        return resources.client().resource(url).type(MediaType.APPLICATION_JSON_TYPE);
     }
 }
