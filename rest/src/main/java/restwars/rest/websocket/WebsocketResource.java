3bf91d852df3e79c9c2d2c10bf482d9bbbe72418
Moritz Kammerer <kammerer.moritz@gmail.com>
2015-02-19 18:44:45 +0100

    Support long polling.

--- rest/src/main/java/restwars/rest/websocket/WebsocketResource.java
+++ rest/src/main/java/restwars/rest/websocket/WebsocketResource.java
@@ -3,6 +3,7 @@ package restwars.rest.websocket;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.atmosphere.annotation.Suspend;
+import org.atmosphere.cpr.AtmosphereResource;
 import org.atmosphere.cpr.BroadcasterFactory;
 import org.atmosphere.cpr.DefaultBroadcaster;
 import org.atmosphere.jersey.SuspendResponse;
@@ -24,12 +25,13 @@ public class WebsocketResource {
     @Context
     private BroadcasterFactory broadcasterFactory;
 
+
     @Suspend
     @GET
     @Path("/round")
-    public SuspendResponse<String> registerForRoundEvent() {
+    public SuspendResponse<String> registerForRoundEvent(@Context AtmosphereResource resource) {
         return new SuspendResponse.SuspendResponseBuilder<String>()
-                .resumeOnBroadcast(false)
+                .resumeOnBroadcast(resource.transport() != AtmosphereResource.TRANSPORT.WEBSOCKET)
                 .broadcaster(broadcasterFactory.lookup(DefaultBroadcaster.class, ROUND_BROADCASTER_NAME, true))
                 .build();
     }
