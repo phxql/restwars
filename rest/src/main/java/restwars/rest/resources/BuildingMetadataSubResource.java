049b09296134ab0dea8622a54623e3457e5c94f7
Moritz Kammerer <kammerer.moritz@gmail.com>
2015-03-08 15:57:06 +0100

    Added endpoints to fetch building metadata for a level range.
    - See #89.

--- rest/src/main/java/restwars/rest/resources/BuildingMetadataSubResource.java
+++ rest/src/main/java/restwars/rest/resources/BuildingMetadataSubResource.java
@@ -19,7 +19,9 @@ import javax.annotation.Nullable;
 import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 /**
@@ -48,18 +50,39 @@ public class BuildingMetadataSubResource {
      */
     @GET
     @ApiOperation("Lists metadata for all buildings")
-    public BuildingsMetadataResponse all(@QueryParam("level") @ApiParam(value = "Building level", defaultValue = "1") int level) {
+    public BuildingsMetadataResponse all(@QueryParam("level") @ApiParam(value = "Building level") @DefaultValue("1") int level) {
         int sanitizedLevel = Math.max(level, 1);
 
         return new BuildingsMetadataResponse(Stream.of(BuildingType.values())
                 .map(t -> getMetadata(t, sanitizedLevel)).collect(Collectors.toList()));
     }
 
+    @GET
+    @Path("/range")
+    @ApiOperation("Lists metadata for all buildings within a level range")
+    public BuildingsMetadataResponse all(
+            @QueryParam("levelFrom") @ApiParam(value = "Start of building level range (inclusive)") int levelFrom,
+            @QueryParam("levelTo") @ApiParam(value = "End of building level range (inclusive)") int levelTo
+    ) {
+        int sanitizedLevelFrom = Math.max(levelFrom, 1);
+        int sanitizedLevelTo = Math.max(levelTo, 1);
+
+        if (sanitizedLevelTo < sanitizedLevelFrom) {
+            throw new ParameterValueWebException("levelTo must be >= levelFrom");
+        }
+
+        List<BuildingMetadataResponse> result = IntStream.range(sanitizedLevelFrom, sanitizedLevelTo + 1).
+                boxed().flatMap(level -> Stream.of(BuildingType.values()).map(t -> getMetadata(t, level)))
+                .collect(Collectors.toList());
+
+        return new BuildingsMetadataResponse(result);
+    }
+
     /**
      * Lists metadata for the building with the given type.
      *
      * @param type  Building type.
-     * @param level Building level for which the resource cost and build time should be returned.
+     * @param level Building level for which the building metadata should be returned.
      * @return Metadata for the building.
      */
     @GET
@@ -67,7 +90,8 @@ public class BuildingMetadataSubResource {
     @ApiOperation("Lists metadata for a building")
     public BuildingMetadataResponse one(
             @PathParam("type") @ApiParam(value = "Building type") String type,
-            @QueryParam("level") @ApiParam(value = "Building level", defaultValue = "1") int level) {
+            @QueryParam("level") @ApiParam(value = "Building level") @DefaultValue("1") int level
+    ) {
         int sanitizedLevel = Math.max(level, 1);
 
         try {
@@ -77,6 +101,35 @@ public class BuildingMetadataSubResource {
         } catch (IllegalArgumentException e) {
             throw new BuildingTypeNotFoundWebException();
         }
+    }
+
+    @GET
+    @Path("/{type}/range")
+    @ApiOperation("Lists metadata for a building within a level range")
+    public BuildingsMetadataResponse oneRange(
+            @PathParam("type") @ApiParam(value = "Building type") String type,
+            @QueryParam("levelFrom") @ApiParam(value = "Start of building level range (inclusive)") int levelFrom,
+            @QueryParam("levelTo") @ApiParam(value = "End of building level range (inclusive)") int levelTo
+    ) {
+        int sanitizedLevelFrom = Math.max(levelFrom, 1);
+        int sanitizedLevelTo = Math.max(levelTo, 1);
+
+        if (sanitizedLevelTo < sanitizedLevelFrom) {
+            throw new ParameterValueWebException("levelTo must be >= levelFrom");
+        }
+
+        BuildingType buildingType;
+        try {
+            buildingType = BuildingType.valueOf(type);
+        } catch (IllegalArgumentException e) {
+            throw new BuildingTypeNotFoundWebException();
+        }
+
+        List<BuildingMetadataResponse> result = IntStream.range(sanitizedLevelFrom, sanitizedLevelTo + 1).
+                mapToObj(level -> getMetadata(buildingType, level))
+                .collect(Collectors.toList());
+
+        return new BuildingsMetadataResponse(result);
     }
 
     private BuildingMetadataResponse getMetadata(BuildingType type, int level) {
